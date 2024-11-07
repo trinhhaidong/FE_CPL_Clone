c16e7f5a1c03af37bc768d8150d67ab71385a3b6
TungLam_1286 <lamnt140303@gmail.com>
2024-11-07 16:14:51 +0700

    update footer, header, homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomePageComponent } from '../views/Pages/home-page/home-page.component';
+import { FooterComponent } from '../views/layouts/footer/footer.component';
+import { HeaderComponent } from '../views/layouts/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomePageComponent,FooterComponent,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'RentalCar_System_FE';
+
 }
