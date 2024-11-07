eae6c86c2607a4929372b710e79361ccede2fc27
TungLam_1286 <lamnt140303@gmail.com>
2024-11-07 13:16:53 +0700

    create FE

---
+++ src/views/Cars/car-grid/car-grid.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-grid',
+  standalone: true,
+  imports: [],
+  templateUrl: './car-grid.component.html',
+  styleUrl: './car-grid.component.scss'
+})
+export class CarGridComponent {
+
+}
