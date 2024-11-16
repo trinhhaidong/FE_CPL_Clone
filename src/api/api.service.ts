c6c4abc29511bda6653867ba8dee959c2488fa31
XuanHung <xuanhungdz3011@gmail.com>
2024-11-17 03:26:01 +0700

    Update api.service.ts

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { data } from 'jquery';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -19,5 +20,8 @@ export class ApiService {
 
   signup(data: any): Observable<any>{
     return this.http.post<any>('https://localhost:44360/api/General/register', data);
+  }
+  getCars(): Observable<any>{
+    return this.http.get<any>('https://localhost:44360/api/Car/all-car');
   }
 }
